01eefc8441969ba9ffa69c2bbe193dae1b8afc06
Can Toraman <toraman@usc.edu>
2022-04-13 23:43:21 -0700

    warming up

--- contracts/CrowdfundedToken.sol
+++ contracts/CrowdfundedToken.sol
@@ -8,32 +8,46 @@ contract CrowdfundedToken {
     }
 
     struct CrowdfundedNFT {
+        uint256 _id;
         address seller;
-        address token;
         string name;
         uint floorPrice;
         uint numSharesFunded;
+        uint numSharesTotal;
+        CrowdfundedTokenListingStatus status;
+        constructor(uint256 _index, address _seller, string _name, uint256 _floorPrice, uint256 _numSharesFunded, uint256 _numSharesTotal) public {
+            _id = _index;
+            seller = _seller;
+            name = _name;
+            floorPrice = _floorPrice;
+            numSharesFunded = _numSharesFunded;
+            numSharesTotal = _numSharesTotal;
+            status = CrowdfundedTokenListingStatus.active;
+        }
     }
+    uint256 public numAssets = 0;
+
+    mapping (address => mapping (uint256 => CrowdfundedNFT)) private ownershipData;
+
+    function createCrowdfundingToken(string name, uint floorPrice, uint numSharesTotal) public {
+
+        // first, check if floor price & num shares are valid
+        
+        // instentiate the asset & add to global mapping
+        numAssets++;
+        CrowdfundedNFT newAsset = CrowdfundedNFT(numAssets, msg.sender, name, floorPrice, 0, numSharesTotal, status);
+        ownershipData[msg.sender][newCrowdfundedNFT._id] = newCrowdfundedNFT;
+    }    
+
+    // function purchaseFragment(uint listingId, uint numShares) external {
+    //     CrowdfundedAsset listing = crowdfunded_assets[listingId];
+    //     listing.num_shares_funded += num_shares;
+
+    //     CrowdfundedNFT storage nft = _listings[listingId];
+
+    //     require(nft.numSharesFunded === nft.floorPrice, "Listing is not active");
+    // }
+}
 
     // memory will copy -> Listing memory listing = _listings[index];
     // storage will assign a pointer so it will affect the real value -> Listing storage listing = _listings[index];
-    
-    uint private _listingId = 0;
-    mapping(uint => CrowdfundedNFT) private _listings;
-
-    function listCrowdfundingToken(address token, string name, uint floorPrice) external {
-        CrowdfundedNFT asset = CrowdfundedAsset(msg.sender, token, name, floorPrice, 0);
-        _listingId++;
-        _crowdfundedListings[_listingId] = asset;
-    }
-
-    function purchaseFragment(uint listingId, uint numShares) external {
-        CrowdfundedAsset listing = crowdfunded_assets[listingId];
-        listing.num_shares_funded += num_shares;
-
-        CrowdfundedNFT storage nft = _listings[listingId];
-
-        require(nft.numSharesFunded === nft.floorPrice, "Listing is not active");
-
-    }
-}
